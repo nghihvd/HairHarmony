dad3303fec301f0281a4f461db12986ff1a7e2f3
Đông Nghi <82652596+nghihvd@users.noreply.github.com>
2024-10-04 21:13:20 +0700

    change

--- PRN212_HairHarmony/obj/Debug/net6.0-windows/LoginWindow.g.i.cs
+++ PRN212_HairHarmony/obj/Debug/net6.0-windows/LoginWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace PRN212_HairHarmony {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PRN212_HairHarmony;component/loginwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PRN212_HairHarmony;V1.0.0.0;component/loginwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\LoginWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
